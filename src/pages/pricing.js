3e0006571673e914c5ca2a0c8a454d3bba4d5452
TrevorMicek <miceking@comcast.net>
2022-10-19 16:15:28 -0600

    fixed pricing page tagline

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -9,7 +9,7 @@ import Pricing from '../components/tailwindComps/Pricing'
 
 const ServicePage = () => (
 
-  <Layout title="Pricing" text="Affordable packages with upfront costs and no monthly costs" pageLayout={{gridTemplateRows: "[top-gutter] auto [top-padding] auto [first] auto [second] auto [third] auto [fourth] auto [fifth] auto [sixth] auto [seventh] auto"}}>
+  <Layout title="Pricing" text="Affordable packages with no monthly costs" pageLayout={{gridTemplateRows: "[top-gutter] auto [top-padding] auto [first] auto [second] auto [third] auto [fourth] auto [fifth] auto [sixth] auto [seventh] auto"}}>
     <SEO title="Pricing" />
     <Pricing />
 
